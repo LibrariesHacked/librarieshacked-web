ca4fcc17f38bebf6cc6d9bcb20400eac91fa28a6
Dave <david_rowe@bathnes.gov.uk>
2016-05-21 23:12:27 +0100

    Fixing date

--- config/config.php
+++ config/config.php
@@ -20,7 +20,7 @@ $config['twig_config'] = array(		// Twig settings
 /*
  * CONTENT
  */
-$config['date_format'] = '%D %T';		// Set the PHP date format as described here: http://php.net/manual/en/function.strftime.php
+$config['date_format'] = '%a %#d %b';	// Set the PHP date format as described here: http://php.net/manual/en/function.strftime.php
 $config['pages_order_by'] = 'date';		// Order pages by "alpha" or "date"
 $config['pages_order'] = 'desc';		// Order pages "asc" or "desc"
 $config['content_dir'] = 'content/';	// Content directory
