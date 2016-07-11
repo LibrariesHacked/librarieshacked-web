e2940978475b57be718dd565ea6c84613bc73485
Dave <david_rowe@bathnes.gov.uk>
2016-07-11 21:36:51 +0100

    working through redesign bugs

--- themes/bootstrap/js/tutorialpage_bristollibraries.js
+++ themes/bootstrap/js/tutorialpage_bristollibraries.js
@@ -1,21 +1,30 @@
-var closingCircles, libraryCircles, map, closingDisplayed = false;
+var closingCircles, bristolBoundary, bristolLibraries, libraryCircles, map, closingDisplayed = false;
 
 $(function () {
-    // set up the leaflet map.  initial view of Bristol centre.
-    map = L.map('map').setView([51.45, -2.60], 12);
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x' : '') + '.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OSM</a> contributors, <a href="http://cartodb.com/attributions">CartoDB</a>'
-    }).addTo(map);
+    $.when(
+        $.getJSON('/data/Boundary_Bristol.json', function (data) {
+            bristolBoundary = data;
+        }),
+        $.getJSON('/data/Libraries_Bristol.json', function (data) {
+            bristolLibraries = data;
+        })
+    ).then(function () {
+        // set up the leaflet map.  initial view of Bristol centre.
+        map = L.map('map').setView([51.45, -2.60], 12);
+        L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x' : '') + '.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OSM</a> contributors, <a href="http://cartodb.com/attributions">CartoDB</a>'
+        }).addTo(map);
 
-    // add the boundary layer for Bristol.
-    L.geoJson(bristolBoundary, {
-        weight: 2, fill: true, fillOpacity: 0.7, fillColor: '#FFF', color: '#999999', clickable: false,
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, { opacity: 0, fillOpacity: 0, clickable: false });
-        }
-    }).addTo(map);
-    // By default use 1609 metres.  
-    addLayers(1609);
+        // add the boundary layer for Bristol.
+        L.geoJson(bristolBoundary, {
+            weight: 2, fill: true, fillOpacity: 0.7, fillColor: '#FFF', color: '#999999', clickable: false,
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, { opacity: 0, fillOpacity: 0, clickable: false });
+            }
+        }).addTo(map);
+        // By default use 1609 metres.  
+        addLayers(1609);
+    });
 });
 
 function showProposedLibraries() {
