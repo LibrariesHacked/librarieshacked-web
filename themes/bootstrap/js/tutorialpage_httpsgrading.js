96c51d4ac8fc62f473a78dadaff4ae9606dffb13
Dave <david_rowe@bathnes.gov.uk>
2016-07-17 16:51:24 +0100

    HTTPS Grading custom page

---
+++ themes/bootstrap/js/tutorialpage_httpsgrading.js
@@ -0,0 +1,19 @@
+$(function () {
+    Papa.parse('/data/UK_Catalogues_Security.csv', {
+        download: true,
+        complete: function (results) {
+            var dataSet = results.data.slice(1);
+            $('#tblCatalogues').DataTable({
+                data: dataSet,
+                columns: [
+                    { title: "Library service", data: 0 },
+                    { title: "Uses HTTPS", data: 2 },
+                    { title: "Issues?", data: 3 },
+                    { title: "Web domain", data: 1 }
+                ],
+                responsive: true,
+                bAutoWidth: false
+            });
+        }
+    });
+});
