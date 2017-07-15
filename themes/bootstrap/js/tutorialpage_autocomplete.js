92c604627fbb987e93517a6da9961036d053ad62
Dave <dave_roweuk@yahoo.co.uk>
2017-07-15 08:55:57 +0100

    Fixing things before new version

--- themes/bootstrap/js/tutorialpage_autocomplete.js
+++ themes/bootstrap/js/tutorialpage_autocomplete.js
@@ -30,28 +30,28 @@ $(function () { // only begin once page has loaded
         },
         select: function (event, ui) {
             // clear down from previous book.
-            $('#divBook').empty();
-            $('#divDescription').empty();
-            $('#aWorldCat').addClass('disabled');
-            $('#divBookDetails').show();
-            if (ui.item.image != '') $('#divBook').append('<img src="' + ui.item.image + '" style="float: left; padding: 10px;">');
+            $('#div-book div').empty();
+            $('#div-description div').empty();
+            $('#a-worldCat').addClass('disabled');
+            $('#div-book-details').show();
+            if (ui.item.image != '') $('#div-book-details #book div').append('<img src="' + ui.item.image + '" style="float: left; padding: 10px;">');
             // and title, author, and year
-            $('#divBook').append('<h4> ' + ui.item.title + '</h4>');
-            $('#divBook').append(ui.item.author + '<br>');
-            $('#divBook').append(ui.item.publishedDate);
+            $('#div-book-details #book div').append('<h4> ' + ui.item.title + '</h4>');
+            $('#div-book-details #book div').append(ui.item.author + '<br>');
+            $('#div-book-details #book div').append(ui.item.publishedDate);
             // and the usual description of the book
-            $('#pDescription').text(ui.item.description);
+            $('#div-book-details #description div').text(ui.item.description);
             // and show the link to oclc (if we have an isbn number)
             if (ui.item.isbn && ui.item.isbn[0].identifier) {
-                $('#aWorldCat').removeClass('disabled');
-                $('#aWorldCat').attr("href", 'http://www.worldcat.org/isbn/' + ui.item.isbn[0].identifier);
-                $('#aOpenLibrary').removeClass('disabled');
-                $('#aOpenLibrary').attr("href", 'https://openlibrary.org/isbn/' + ui.item.isbn[0].identifier);
+                $('#a-worldcat').removeClass('disabled');
+                $('#a-worldcat').attr("href", 'http://www.worldcat.org/isbn/' + ui.item.isbn[0].identifier);
+                $('#a-openlibrary').removeClass('disabled');
+                $('#a-openlibrary').attr("href", 'https://openlibrary.org/isbn/' + ui.item.isbn[0].identifier);
             }
         },
         minLength: 2, // set minimum length of text the user must enter
         open: function () {
-            var txt = $('#txtBookSearch');
+            var txt = $('#txt-search');
             var width = txt.outerWidth();
             $('.ui-autocomplete').width(width);
         }
--- themes/bootstrap/js/tutorialpage_barcode.js
+++ themes/bootstrap/js/tutorialpage_barcode.js
@@ -10,21 +10,7 @@
 var getBarcode = function(accountNumber) {
     $('#demo').barcode(
         accountNumber, // Value barcode (dependent on the type of barcode)
-        'code39', // type (string)
-        {
-            barWidth: 1,
-            barHeight: 60,
-            moduleSize: 5,
-            showHRI: true,
-            addQuietZone: true,
-            marginHRI: 5,
-            bgColor: '#FFFFFF',
-            color: '#000000',
-            fontSize: 10,
-            output: 'bmp',
-            posX: 0,
-            posY: 0
-        }
+        'code39'
     );
 };
 
@@ -33,5 +19,5 @@ var getBarcode = function(accountNumber) {
 ///////////////////////////////////////////////////////////
 $(function(){
     // Attach to the relevant page control
-    $('#').on('click', function(e){ getBarcode(); });
+    $('#btncreatebarcode').on('click', function(e){ getBarcode($('#txtaccountnumber').val()); });
 });
