343b57b0ce270d6528fd7eaa2c88a5cad57e9ee5
Dave <dave_roweuk@yahoo.co.uk>
2017-08-25 20:22:15 +0100

    Updating tables and removing twitter stuff

--- themes/bootstrap/js/custom.js
+++ themes/bootstrap/js/custom.js
@@ -8,10 +8,8 @@ var sitecolours = {
 };
 
 $(function () {
-    // $('[data-toggle="popover"]').popover();
-    
     // Don't like this much but prefer writing tables in markdown so need to add a standard set of bootstrap classes
-    $('table').addClass('table');
+    $('table').addClass('table table-bordered');
     $('blockquote').addClass('blockquote');
 });
 
